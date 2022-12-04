a4b892592aaa0875ec0f2747d32db22fad35c35c
dimitripez <dimitri@noumena.global>
2022-12-04 15:15:57 -0600

    Initial Commit

---
+++ FrisbeeGolf/FrisbeeGolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrisbeeGolfApp.swift
+//  FrisbeeGolf
+//
+//  Created by Demetrios Pezaris on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct FrisbeeGolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
